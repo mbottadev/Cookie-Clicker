5208789c3cec4a5f44699fb5125bb62d5fb829d0
Pierre-Louis242 <plpicard@hotmail.fr>
2019-02-06 16:04:37 +0100

    test

--- main.js
+++ main.js
@@ -12,14 +12,17 @@ let B2 = document.getElementById("B2");
 let B3 = document.getElementById("B3");
 let B4 = document.getElementById("B4");
 let total = document.getElementById("total");
-let scoreJs = 0
 
+let scoreJs = 0
+let B2Js = 1 
 
 cookie.addEventListener("click", function(){
-    scoreJs ++
+    scoreJs = scoreJs + multiplicateur 
     score.innerHTML = "Le score est de \n " + scoreJs 
+
 })
 
-
+B2.addEventListener()
 
 }
+blah blah 
