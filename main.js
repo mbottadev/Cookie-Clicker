daab9c1dbe053f4a251d36cd135d7f101410a1b9
Pierre-Louis242 <plpicard@hotmail.fr>
2019-02-06 15:34:08 +0100

    CookieV1

---
+++ main.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+
+let score = document.getElementById("score");
+let cps = document.getElementById("cps");
+let cookie = document.querySelector("#cookie");
+let checkB1 = document.getElementById("checkB1");
+let checkB2 = document.getElementById("checkB2");
+let checkB3 = document.getElementById("checkB3");
+let checkB4 = document.getElementById("checkB4");
+let B1 = document.getElementById("B1");
+let B2 = document.getElementById("B2");
+let B3 = document.getElementById("B3");
+let B4 = document.getElementById("B4");
+let total = document.getElementById("total");
+let scoreJs = 0
+
+
+cookie.addEventListener("click", function(){
+    scoreJs ++
+    score.innerHTML = "Le score est de \n " + scoreJs 
+})
+
+
+
+}
